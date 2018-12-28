7e587c663f50d45bed8c277ed6a84073db9af716
zeonsunlightyu <38597935+zeonsunlightyu@users.noreply.github.com>
2018-12-28 19:01:57 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,10 @@
+from keras import optimizers
+from keras import losses
 
+noise_level = 0.15
+epoch = 1000
+batch_size = 128
+opt = optimizers.SGD(lr=0.003, momentum=0.0, decay=0.995, nesterov=False)
+loss = losses.mean_squared_error
+lr = 0.003
+wdecay = 0.995

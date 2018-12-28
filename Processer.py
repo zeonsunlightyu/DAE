56f79fcbeb22c1ab2c787b57d4e6911a1d42c1dd
zeonsunlightyu <38597935+zeonsunlightyu@users.noreply.github.com>
2018-12-28 18:48:58 +0800

    Update Processer.py

--- Processer.py
+++ Processer.py
@@ -1,7 +1,13 @@
+import numpy as np
+import pandas as pd
+from scipy.special import erfinv
+import re
+
 class Processer(object):
     """
     dataframe 处理类的方法
     """
+    @staticmethod
     def drop_columns(df, col_names):
         """
         删除不需要的列
@@ -14,7 +20,7 @@ class Processer(object):
         df = df.drop(col_names, axis = 1)
         print('After drop columns {0}'.format(df.shape))
         return df
-    
+    @staticmethod
     def dtype_transform(df):
         """
         数据类型转换
@@ -26,7 +32,7 @@ class Processer(object):
         for col in df.select_dtypes(include=['int64']).columns:
             df[col] = df[col].astype(np.int8)
         return df
-    
+    @staticmethod
     def label_encode(df,col_names):
         """
         对非数值的特征进行编码
@@ -36,7 +42,7 @@ class Processer(object):
         df = preprocessing.LabelEncoder().fit(df[col_names]).transform(df[col_names])
         
         return df
-    
+    @staticmethod
     def fill_missing(df,col_names,fill_values):
         #SimpleImputer(missing_values=np.nan, strategy=method,fill_value=fill_value)
         """
@@ -54,8 +60,8 @@ class Processer(object):
     """
     变量变换类的方法（离散化，onehot，）
     """
-    
-    def onehot(df,cat_features,drop_origin = True,threshold=5):
+    @staticmethod
+    def onehot(df,cat_features,drop_origin = True,threshold=0):
         """
         one hot encoding
         df:类型为Dataframe的数据集
@@ -65,6 +71,7 @@ class Processer(object):
         """
         print('Before ohe : dataframe {0}'.format(df))
         for column in cat_features:
+            print("the number of unique number(exclude threhold): {0}".format(len(df[column].unique())))
             dummy_column = pd.get_dummies(pd.Series(df[column]), prefix=column)
             abort_columns = []
             for col in dummy_column:
@@ -75,5 +82,32 @@ class Processer(object):
             remain_cols = [c for c in dummy_column.columns if c not in abort_columns]
             df = pd.concat([df,dummy_column[remain_cols]],axis=1)
             if drop_origin:
+                print("Drop column : {0}".format(column))
                 df = df.drop([column], axis=1)
+        return df
+    
+    @staticmethod
+    def rank_guass(df,col_names):
+        """
+        rank_guass
+        """
+        for c in col_names:
+            series = df[c].rank()
+            M = series.max()
+            m = series.min() 
+            print(c, m, len(series), len(set(df[c].tolist())))
+            series = (series-m)/(M-m)
+            series = series - series.mean()
+            series = series.apply(erfinv) 
+            df[c] = series
+            
+        return df
+
+class Compose(object):
+    def __init__(self, transforms_params):
+        self.transforms_params = transforms_params
+    def __call__(self, df):
+        for transform_param in self.transforms_params:
+            transform, param = transform_param[0], transform_param[1]
+            df = transform(df, **param)
         return df

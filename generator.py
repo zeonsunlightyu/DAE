33abb23cf0e00655c4feb25cb7eb0263ad2cc2cc
zeonsunlightyu <38597935+zeonsunlightyu@users.noreply.github.com>
2018-12-28 22:31:30 +0800

    Update generator.py

--- generator.py
+++ generator.py
@@ -5,7 +5,7 @@ def add_swap_noise(cur_data, all_data, noise_level=0.15):
     batch_size = cur_data.shape[0]
     num_samples = all_data.shape[0]
     num_features = cur_data.shape[1]
-    #可能会导致同一个batch内部的互换,虽然概率应该很低
+
     random_row = np.random.randint(0, num_samples, size=batch_size)
     for i in range(batch_size):
         random_swap = np.random.rand(num_features) < noise_level

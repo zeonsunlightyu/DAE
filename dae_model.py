322a780b8c003ea441356a8f83460de4c685702e
zeonsunlightyu <38597935+zeonsunlightyu@users.noreply.github.com>
2018-12-28 15:14:50 +0800

    Update dae_model.py

--- dae_model.py
+++ dae_model.py
@@ -1,29 +1,39 @@
-from utils import *
+from config import *
 from keras.models import Model
 from keras.layers import Dense,Input,Activation
 from keras.layers.advanced_activations import PReLU
 from keras import optimizers
 from keras import losses
+from generator import *
 
-def nn_model():
+class NN_model(Object):
+    def __init__(self,X):
+        self.noise_level = config.noise_level
+        self.epoch = config.epoch
+        self.batch_size = config.batch_size
+        self.opt = config.opt
+        self.loss = config.loss
+        self.lr = config.lr
+        self.wdecay = config.wdecay
+        self.X = X
+        self.model = self.bulid_model()
+        self.data_generator = swap_noise_generator(data = self.X,batch_szie = )
+        
+    def build_model(self):
+        
+        inputs = Input((self.X.shape[1],))
+        x = Dense(1500, activation='relu')(inputs)
+        x = Dense(1500, activation='relu')(x)
+        x = Dense(1500, activation='relu')(x)
+        outputs = Dense(self.X.shape[1], activation='relu')(x)
+        model = Model(inputs=inputs, outputs=outputs)
+        model.compile(optimizer=self.opt, loss=self.loss)
+        
+        return model
     
-    input = Input(shape = (221,), dtype='float32')
-    
-    fc1_encode = Dense(1500,init = 'he_normal',activation='relu')(input)
-    fc_bottle_neck = Dense(1500,init = 'he_normal',activation='relu')(fc1_encode)
-    fc1_decode = Dense(1500,init = 'he_normal',activation='relu')(fc_bottle_neck)
-    
-    output = Dense(221,activation='linear')(fc1_decode)
-    
-    sgd = optimizers.SGD(lr=0.003, decay=0.995)
-    model = Model(input = input, output = output)
-    model.compile(loss = losses.mean_squared_error,
-                  optimizer = sgd)
-    
-    return model
-  
-
-    
-    
-  
-
+    def fit(self):
+        
+        self.model.fit_generator(generator=self.data_generator,
+                  steps_per_epoch=np.ceil(self.X.shape[0] / self.batch_size),
+                  epochs=self.epoch,
+                  verbose=1)

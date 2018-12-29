e7a5962a4804e6fb0d265dd4109ddcbe38cd2977
zeonsunlightyu <38597935+zeonsunlightyu@users.noreply.github.com>
2018-12-29 16:26:37 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 from keras import optimizers
 from keras import losses
-
-noise_level = 0.15
+from keras import initializers
+from math import *
+noise_level = 0.07
 epoch = 1000
 batch_size = 128
-opt = optimizers.SGD(lr=0.003, momentum=0.0, decay=0.995, nesterov=False)
-loss = losses.mean_squared_error
 lr = 0.003
 wdecay = 0.995
+opt = optimizers.Adam(lr=lr,decay=wdecay)#,momentum=0.9, nesterov=True)
+loss = 'mean_squared_error'
+init = initializers.RandomUniform(minval=-sqrt(1.0/221), maxval=sqrt(1.0/221), seed=12345)
+metric = 'mse'

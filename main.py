8654a3672b119b8aff24d98bb1e169cf4a217187
zeonsunlightyu <38597935+zeonsunlightyu@users.noreply.github.com>
2018-12-29 16:28:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ def load_data():
     df_test = pd.read_csv('test.csv')
     
     df = pd.concat([df_train, df_test], axis=0)
-    df = df.fillna(-1)
+    #df = df.fillna(-1)
     
     cat_list = []
     calc_list = []
-    no_calc_list = []
+    no_calc_bin_list = []
     non_features_list = ['id','target']
     
     for col in df.columns:
@@ -22,26 +22,30 @@ def load_data():
         if re.search(r'calc', col):
             calc_list.append(col)
         else:
-            no_calc_list.append(col)
+            if not re.search(r'bin',col):
+                no_calc_bin_list.append(col)
             if re.search(r'cat$', col):
                 cat_list.append(col)
-    
+
+    print("the cat list : {0}".format(cat_list))
+    print("the calc list : {0}".format(calc_list))
+    print("the no calc no bin list : {0}".format(no_calc_bin_list))
+
     transformer_one = [
-    (Processer.drop_columns, dict(col_names=calc_list)),
-    (Processer.drop_columns, dict(col_names=non_features_list)),
-    (Processer.dtype_transform, dict()),
-    (Processer.onehot,dict(cat_features = cat_list,drop_origin=False)),
-    (Processer.rank_guass,dict(col_names = no_calc_list)),
+    (drop_columns, dict(col_names=calc_list)),
+    (drop_columns, dict(col_names=non_features_list)),
+    (dtype_transform, dict()),
+    (onehot,dict(cat_features = cat_list,drop_origin=False)),
+    (rank_guass,dict(col_names = no_calc_bin_list))
     ]
     df = Compose(transformer_one)(df)
-    
     print("the shape of dataframe : {0}".format(df.shape))
     
     return df
 
 def main():
-     
+    np.random.seed(12345)
     df = load_data()
-    nn = NN_model(df)
-    nn.fit()
+    nn = NN_model(df.values)
+    nn.fit_generator()
 main()

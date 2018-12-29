d7edd0215b807fc8ff8abdd74e6060479fcfc10c
zeonsunlightyu <38597935+zeonsunlightyu@users.noreply.github.com>
2018-12-29 16:26:00 +0800

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 def add_swap_noise(cur_data, all_data, noise_level=0.15):
-  
+    
+    batch = cur_data.copy()
     batch_size = cur_data.shape[0]
     num_samples = all_data.shape[0]
     num_features = cur_data.shape[1]
@@ -9,10 +10,10 @@ def add_swap_noise(cur_data, all_data, noise_level=0.15):
     random_row = np.random.randint(0, num_samples, size=batch_size)
     for i in range(batch_size):
         random_swap = np.random.rand(num_features) < noise_level
-        cur_data[i, random_swap] = all_data[random_row[i], random_swap]
-    return cur_data
+        batch[i, random_swap] = all_data[random_row[i], random_swap]
+    return batch
 
-def batch_generator(data, batch_size, shuffle=True):
+def batch_generator(data, batch_size, shuffle=True,noise_level=0.15):
     
     batch_index = 0
     n = data.shape[0]
@@ -23,7 +24,8 @@ def batch_generator(data, batch_size, shuffle=True):
             index_array = np.arange(n)
             if shuffle:
                 index_array = np.random.permutation(n)
-            
+                data = data[index_array]
+
         current_index = (batch_index * batch_size) % n
         if n >= current_index + batch_size:
             current_batch_size = batch_size
@@ -32,16 +34,15 @@ def batch_generator(data, batch_size, shuffle=True):
             current_batch_size = n - current_index
             batch_index = 0
 
-        batch = data[index_array[current_index: current_index + current_batch_size]]
-        
-        yield batch
+        batch = data[current_index: current_index + current_batch_size]
+        batch_noise = add_swap_noise(batch, data, noise_level=noise_level)
+
+        yield (batch_noise,batch)
 
 def swap_noise_generator(data,batch_size,shuffle=True,noise_level=0.15):
     
-    gen = batch_generator(data, batch_size, shuffle)
+    gen = batch_generator(data, batch_size, shuffle,noise_level=noise_level)
+
     while True:
-        batch = next(gen)
-        y = batch.copy()
-        x = add_swap_noise(batch, data, noise_level=noise_level)
-        
-        yield (x,y)
+
+        yield next(gen)

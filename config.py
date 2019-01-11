ddea77dfdb6bdb1582b5ab3915a3a8d51e6a3d07
zeonsunlightyu <38597935+zeonsunlightyu@users.noreply.github.com>
2019-01-11 15:17:10 +0800

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ epoch = 1000
 batch_size = 128
 lr = 0.003
 wdecay = 0.995
-opt = optimizers.Adam(lr=lr,decay=wdecay)#,momentum=0.9, nesterov=True)
+opt = optimizers.SGD(lr=lr,decay=wdecay)#,momentum=0.9, nesterov=True)
 loss = 'mean_squared_error'
 init = initializers.RandomUniform(minval=-sqrt(1.0/221), maxval=sqrt(1.0/221), seed=12345)
 metric = 'mse'

0d1f24cfb295c0f95a9c08f3d18e11a0d14f0902
zeonsunlightyu <38597935+zeonsunlightyu@users.noreply.github.com>
2019-01-11 15:49:23 +0800

    Update config.py

--- config.py
+++ config.py
@@ -9,5 +9,8 @@ lr = 0.003
 wdecay = 0.995
 opt = optimizers.SGD(lr=lr,decay=wdecay)#,momentum=0.9, nesterov=True)
 loss = 'mean_squared_error'
-init = initializers.RandomUniform(minval=-sqrt(1.0/221), maxval=sqrt(1.0/221), seed=12345)
+kernel_initializer_0=initializers.RandomNormal(mean=-4.74564e-05, stddev=0.0388202, seed=None)   
+kernel_initializer_1=initializers.RandomNormal(mean=8.51905e-06, stddev=0.0148989, seed=None)   
+kernel_initializer_2=initializers.RandomNormal(mean=8.51905e-06, stddev=0.0148989, seed=None)    
+kernel_initializer_3=initializers.RandomNormal(mean=-1.80977e-05, stddev=0.0149005, seed=None)   
 metric = 'mse'
